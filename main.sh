c3bc8b824d5ec662b0d838f6e15bfcf9d31606bc
china-digital-times <cdt@chinadigitaltimes.net>
2019-05-06 21:15:21 -0400

    media list

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-n=1724
-offset=174112
-max=174700
+n=0
+offset=0
+max=171093
 
 while [ $offset -le $max ]; do
-    curl -s -o "../list/$n.json" "https://chinadigitaltimes.net/chinese/wp-json/wp/v2/posts/?order=asc&per_page=100&orderby=id&offset=${offset}"
+    curl -s -o "../media-list/$n.json" "https://chinadigitaltimes.net/chinese/wp-json/wp/v2/media/?order=asc&per_page=100&orderby=id&offset=${offset}"
     echo "$n.json"
     offset=$(($offset+100))
     n=$(($n+1))
